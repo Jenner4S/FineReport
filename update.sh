4b86ada369c3f8eac3000b1b96088643c24f394f
finereport <develop@finereport.com>
2015-11-11 10:47:22 +0800

    最新的jar

--- update.sh
+++ update.sh
@@ -3,3 +3,14 @@ svn up designer
 svn up designer_base
 svn up designer_chart
 svn up designer_form
+ftp -n<<!
+open 192.168.100.98
+user anonymous anonymous
+binary
+cd /8.0/CN
+lcd lib
+prompt
+mget fr-core-8.0.jar fr-chart-8.0.jar fr-report-8.0.jar fr-platform-8.0.jar fr-performance-8.0.jar fr-third-8.0.jar 
+close
+bye
+!
