f949169c477f154156d0f40e2e670b1379ed0047
richie <richie@finereport.com>
2016-01-20 10:35:33 +0800

    更新jar和代码

--- update.sh
+++ update.sh
@@ -3,14 +3,8 @@ svn up designer
 svn up designer_base
 svn up designer_chart
 svn up designer_form
-ftp -n<<!
-open env.finedevelop.com
-user anonymous anonymous
-binary
-cd /8.0/CN
-lcd lib
-prompt
-mget fr-core-8.0.jar fr-chart-8.0.jar fr-report-8.0.jar fr-platform-8.0.jar fr-performance-8.0.jar 
-close
-bye
-!
+curl -o lib/fr-core-8.0.jar http://download.finedevelop.com/fr-core-8.0.jar
+curl -o lib/fr-chart-8.0.jar http://download.finedevelop.com/fr-chart-8.0.jar
+curl -o lib/fr-report-8.0.jar http://download.finedevelop.com/fr-report-8.0.jar
+curl -o lib/fr-platform-8.0.jar http://download.finedevelop.com/fr-platform-8.0.jar
+curl -o lib/fr-performance-8.0.jar  http://download.finedevelop.com/fr-performance-8.0.jar 
