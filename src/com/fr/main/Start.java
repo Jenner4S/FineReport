dc56701b2507321ee54b938ca74458fe09dbf6ec
hao01 <hao01@hao>
2016-02-29 20:44:07 +0800

    1

--- src/com/fr/main/Start.java
+++ src/com/fr/main/Start.java
@@ -1,12 +1,13 @@
 package com.fr.main;
 
 import com.fr.start.Designer;
+import com.fr.start.Designer4Debug;
 
 public class Start {
 	public static void main(String[] str ){
-		Designer.main(str);
+//		Designer.main(str);
 		
-		
+		Designer4Debug.main(str);
 	}
 
 }
