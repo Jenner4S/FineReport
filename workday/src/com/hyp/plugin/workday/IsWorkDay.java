b42a5723f51e5ba46cc2bd0279636f728d225892
hao01 <hao01@hao>
2016-03-03 12:34:49 +0800

    1

--- workday/src/com/hyp/plugin/workday/IsWorkDay.java
+++ workday/src/com/hyp/plugin/workday/IsWorkDay.java
@@ -1,12 +1,33 @@
 package com.hyp.plugin.workday;
 
+import com.fr.general.Inter;
 import com.fr.json.JSONObject;
+import com.fr.plugin.ExtraClassManager;
 import com.fr.script.AbstractFunction;
+import com.fr.stable.fun.FunctionHelper;
+import com.fr.stable.fun.FunctionProcessor;
+import com.fr.stable.fun.impl.AbstractFunctionProcessor;
 
 public class IsWorkDay extends AbstractFunction {
-
+	public static final FunctionProcessor ONEFUNCTION = new AbstractFunctionProcessor(){
+		@Override
+		public int getId(){
+			int id = FunctionHelper.generateFunctionID("com.hyp.plugin.workday");
+			return id;
+		}
+		@Override
+			public String toString(){
+			return Inter.getLocText("Plugin-WorkDay_isworkday");
+		}
+	};
+		
     @Override
     public Object run(Object[] args) {
+    	FunctionProcessor processor=ExtraClassManager.getInstance().getFunctionProcessor();
+    	if(processor!=null){
+    		processor.recordFunction(ONEFUNCTION);
+    	}
+    	
     	if(args!=null){
     			return GetWorkDay.isWorkDay(args);
     	}
