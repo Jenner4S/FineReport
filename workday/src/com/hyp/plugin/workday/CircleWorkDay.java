b199b010bf7a24c82e4afdc723865cb5e77b7115
hao01 <hao01@hao>
2016-03-09 11:52:17 +0800

    1

---
+++ workday/src/com/hyp/plugin/workday/CircleWorkDay.java
@@ -0,0 +1,26 @@
+package com.hyp.plugin.workday;
+
+import com.fr.general.Inter;
+import com.fr.plugin.ExtraClassManager;
+import com.fr.script.AbstractFunction;
+import com.fr.stable.fun.FunctionHelper;
+import com.fr.stable.fun.FunctionProcessor;
+import com.fr.stable.fun.impl.AbstractFunctionProcessor;
+
+public class CircleWorkDay  extends AbstractFunction {
+	
+		
+    @Override
+    public Object run(Object[] args) {
+    	FunctionProcessor processor=ExtraClassManager.getInstance().getFunctionProcessor();
+    	if(processor!=null){
+    		processor.recordFunction(IsWorkDay.ONEFUNCTION);
+    	}
+    	
+    	if(args!=null){
+    			return GetWorkDay.circleWordDay(args);
+    	}
+//    	System.out.println(args[0]);
+    	return false;
+    }
+}
